4b240d5e34d5a918c5001f661c36abe807590cf1
Lorelei Penn <lorelei.penn@pm.me>
2026-01-05 15:00:04 -0600

    Added p10k theme for ascii terminals.

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended "
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 sleep 1s
 
